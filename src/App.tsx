eb931eeed8e9959fd09c790dba787b52510c3a5e
fabio-peres <fabioperesg@gmail.com>
2021-10-28 23:19:20 -0300

    Added reset and global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Routes from './routes';
+import './styles/global.css';
+import './styles/reset.css';
 
 function App() {
   return (    
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/components/navigation-bar.css';
 
 function NavigationBar() {
   return (
-    <nav>
+    <nav className='navbar'>
       <ul>
-        <li>
+        <li className='nav-item'>
           <NavLink to="/home">Inicio</NavLink>
         </li>
-        <li>
+        <li className='nav-item'>
           <NavLink to="/offers">Ofertas</NavLink>
         </li>
       </ul>
